f8286f4738ed843bfb50d72ecc9281de3ec0e9bc
Umar Mohammed <umohammed@caci.co.uk>
2018-11-08 16:58:49 +0000

    add empty dotnetcore project
    #1

---
+++ MsalDemo.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MsalDemo.WebApi.Controllers
+{
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ActionResult Get()
+        {
+            return Ok("Hello world");
+        }
+
+    }
+}
