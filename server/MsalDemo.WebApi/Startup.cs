1ad6c3d5f236a25d680d38d30d39bd38ccee6f1b
Umar Mohammed <umohammed@caci.co.uk>
2018-11-08 17:06:50 +0000

    move directory

--- server/MsalDemo.WebApi/Startup.cs
+++ server/MsalDemo.WebApi/Startup.cs
@@ -15,7 +15,6 @@ namespace MsalDemo.WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,7 +25,10 @@ namespace MsalDemo.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
